3d50db436a1548d731ddddfd82d2cee1695565d8
Albin Kerouanton <albinker@gmail.com>
2024-05-03 20:35:56 +0200

    layer.go: add ctx args to all funcs
    Signed-off-by: Albin Kerouanton <albinker@gmail.com>

--- layer.go
+++ layer.go
@@ -15,64 +15,64 @@ func layerPath(info *DriverInfo, id string) string {
 	return filepath.Join(info.HomeDir, id)
 }
 
-func ActivateLayer(info DriverInfo, id string) error {
-	return wclayer.ActivateLayer(context.Background(), layerPath(&info, id))
+func ActivateLayer(ctx context.Context, info DriverInfo, id string) error {
+	return wclayer.ActivateLayer(ctx, layerPath(&info, id))
 }
-func CreateLayer(info DriverInfo, id, parent string) error {
-	return wclayer.CreateLayer(context.Background(), layerPath(&info, id), parent)
+func CreateLayer(ctx context.Context, info DriverInfo, id, parent string) error {
+	return wclayer.CreateLayer(ctx, layerPath(&info, id), parent)
 }
 
 // New clients should use CreateScratchLayer instead. Kept in to preserve API compatibility.
-func CreateSandboxLayer(info DriverInfo, layerId, parentId string, parentLayerPaths []string) error {
-	return wclayer.CreateScratchLayer(context.Background(), layerPath(&info, layerId), parentLayerPaths)
+func CreateSandboxLayer(ctx context.Context, info DriverInfo, layerId, parentId string, parentLayerPaths []string) error {
+	return wclayer.CreateScratchLayer(ctx, layerPath(&info, layerId), parentLayerPaths)
 }
-func CreateScratchLayer(info DriverInfo, layerId, parentId string, parentLayerPaths []string) error {
-	return wclayer.CreateScratchLayer(context.Background(), layerPath(&info, layerId), parentLayerPaths)
+func CreateScratchLayer(ctx context.Context, info DriverInfo, layerId, parentId string, parentLayerPaths []string) error {
+	return wclayer.CreateScratchLayer(ctx, layerPath(&info, layerId), parentLayerPaths)
 }
-func DeactivateLayer(info DriverInfo, id string) error {
-	return wclayer.DeactivateLayer(context.Background(), layerPath(&info, id))
+func DeactivateLayer(ctx context.Context, info DriverInfo, id string) error {
+	return wclayer.DeactivateLayer(ctx, layerPath(&info, id))
 }
 
-func DestroyLayer(info DriverInfo, id string) error {
-	return wclayer.DestroyLayer(context.Background(), layerPath(&info, id))
+func DestroyLayer(ctx context.Context, info DriverInfo, id string) error {
+	return wclayer.DestroyLayer(ctx, layerPath(&info, id))
 }
 
 // New clients should use ExpandScratchSize instead. Kept in to preserve API compatibility.
-func ExpandSandboxSize(info DriverInfo, layerId string, size uint64) error {
-	return wclayer.ExpandScratchSize(context.Background(), layerPath(&info, layerId), size)
+func ExpandSandboxSize(ctx context.Context, info DriverInfo, layerId string, size uint64) error {
+	return wclayer.ExpandScratchSize(ctx, layerPath(&info, layerId), size)
 }
-func ExpandScratchSize(info DriverInfo, layerId string, size uint64) error {
-	return wclayer.ExpandScratchSize(context.Background(), layerPath(&info, layerId), size)
+func ExpandScratchSize(ctx context.Context, info DriverInfo, layerId string, size uint64) error {
+	return wclayer.ExpandScratchSize(ctx, layerPath(&info, layerId), size)
 }
-func ExportLayer(info DriverInfo, layerId string, exportFolderPath string, parentLayerPaths []string) error {
-	return wclayer.ExportLayer(context.Background(), layerPath(&info, layerId), exportFolderPath, parentLayerPaths)
+func ExportLayer(ctx context.Context, info DriverInfo, layerId string, exportFolderPath string, parentLayerPaths []string) error {
+	return wclayer.ExportLayer(ctx, layerPath(&info, layerId), exportFolderPath, parentLayerPaths)
 }
-func GetLayerMountPath(info DriverInfo, id string) (string, error) {
-	return wclayer.GetLayerMountPath(context.Background(), layerPath(&info, id))
+func GetLayerMountPath(ctx context.Context, info DriverInfo, id string) (string, error) {
+	return wclayer.GetLayerMountPath(ctx, layerPath(&info, id))
 }
-func GetSharedBaseImages() (imageData string, err error) {
-	return wclayer.GetSharedBaseImages(context.Background())
+func GetSharedBaseImages(ctx context.Context) (imageData string, err error) {
+	return wclayer.GetSharedBaseImages(ctx)
 }
-func ImportLayer(info DriverInfo, layerID string, importFolderPath string, parentLayerPaths []string) error {
-	return wclayer.ImportLayer(context.Background(), layerPath(&info, layerID), importFolderPath, parentLayerPaths)
+func ImportLayer(ctx context.Context, info DriverInfo, layerID string, importFolderPath string, parentLayerPaths []string) error {
+	return wclayer.ImportLayer(ctx, layerPath(&info, layerID), importFolderPath, parentLayerPaths)
 }
-func LayerExists(info DriverInfo, id string) (bool, error) {
-	return wclayer.LayerExists(context.Background(), layerPath(&info, id))
+func LayerExists(ctx context.Context, info DriverInfo, id string) (bool, error) {
+	return wclayer.LayerExists(ctx, layerPath(&info, id))
 }
-func PrepareLayer(info DriverInfo, layerId string, parentLayerPaths []string) error {
-	return wclayer.PrepareLayer(context.Background(), layerPath(&info, layerId), parentLayerPaths)
+func PrepareLayer(ctx context.Context, info DriverInfo, layerId string, parentLayerPaths []string) error {
+	return wclayer.PrepareLayer(ctx, layerPath(&info, layerId), parentLayerPaths)
 }
-func ProcessBaseLayer(path string) error {
-	return wclayer.ProcessBaseLayer(context.Background(), path)
+func ProcessBaseLayer(ctx context.Context, path string) error {
+	return wclayer.ProcessBaseLayer(ctx, path)
 }
-func ProcessUtilityVMImage(path string) error {
-	return wclayer.ProcessUtilityVMImage(context.Background(), path)
+func ProcessUtilityVMImage(ctx context.Context, path string) error {
+	return wclayer.ProcessUtilityVMImage(ctx, path)
 }
-func UnprepareLayer(info DriverInfo, layerId string) error {
-	return wclayer.UnprepareLayer(context.Background(), layerPath(&info, layerId))
+func UnprepareLayer(ctx context.Context, info DriverInfo, layerId string) error {
+	return wclayer.UnprepareLayer(ctx, layerPath(&info, layerId))
 }
-func ConvertToBaseLayer(path string) error {
-	return wclayer.ConvertToBaseLayer(context.Background(), path)
+func ConvertToBaseLayer(ctx context.Context, path string) error {
+	return wclayer.ConvertToBaseLayer(ctx, path)
 }
 
 type DriverInfo struct {
@@ -82,8 +82,8 @@ type DriverInfo struct {
 
 type GUID [16]byte
 
-func NameToGuid(name string) (id GUID, err error) {
-	g, err := wclayer.NameToGuid(context.Background(), name)
+func NameToGuid(ctx context.Context, name string) (id GUID, err error) {
+	g, err := wclayer.NameToGuid(ctx, name)
 	return g.ToWindowsArray(), err
 }
 
@@ -100,14 +100,14 @@ func (g *GUID) ToString() string {
 
 type LayerReader = wclayer.LayerReader
 
-func NewLayerReader(info DriverInfo, layerID string, parentLayerPaths []string) (LayerReader, error) {
-	return wclayer.NewLayerReader(context.Background(), layerPath(&info, layerID), parentLayerPaths)
+func NewLayerReader(ctx context.Context, info DriverInfo, layerID string, parentLayerPaths []string) (LayerReader, error) {
+	return wclayer.NewLayerReader(ctx, layerPath(&info, layerID), parentLayerPaths)
 }
 
 type LayerWriter = wclayer.LayerWriter
 
-func NewLayerWriter(info DriverInfo, layerID string, parentLayerPaths []string) (LayerWriter, error) {
-	return wclayer.NewLayerWriter(context.Background(), layerPath(&info, layerID), parentLayerPaths)
+func NewLayerWriter(ctx context.Context, info DriverInfo, layerID string, parentLayerPaths []string) (LayerWriter, error) {
+	return wclayer.NewLayerWriter(ctx, layerPath(&info, layerID), parentLayerPaths)
 }
 
 type WC_LAYER_DESCRIPTOR = wclayer.WC_LAYER_DESCRIPTOR
